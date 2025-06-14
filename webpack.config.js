c65d2187449dab86ec94aad4342195ef2a198f2f
JAIDEEP TAAK <jaideeptaak2@gmail.com>
2025-06-14 18:53:00 +0530

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,18 @@
-var path = require("path");
-const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
-
 module.exports = {
-  entry: {
-    jsnes: "./src/index.js",
-    "jsnes.min": "./src/index.js",
-  },
-  devtool: "source-map",
+  entry: './src/index.js',   // or your actual entry point
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "[name].js",
-    library: "jsnes",
-    libraryTarget: "umd",
-    umdNamedDefine: true,
+    filename: 'bundle.js',
+    path: __dirname + '/dist',
+    libraryTarget: 'commonjs' // << add this
   },
+  target: 'node',             // << add this
   module: {
     rules: [
       {
         test: /\.js$/,
-        enforce: "pre",
         exclude: /node_modules/,
-        use: [
-          {
-            loader: "eslint-loader",
-          },
-        ],
-      },
-    ],
-  },
-  plugins: [
-    new UglifyJsPlugin({
-      include: /\.min\.js$/,
-      sourceMap: true,
-    }),
-  ],
+        use: 'babel-loader'
+      }
+    ]
+  }
 };
